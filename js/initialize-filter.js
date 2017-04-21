232dc4bc24e2bd23b01842474f3e1ac09b73c3f9
Ekaterina.Zharova <Ekaterina.Zharova@jetbrains.com>
2017-04-21 19:42:06 +0300

    #18 Личный проект: уникальный код

---
+++ js/initialize-filter.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function getCoords(elem) {
+  var coords = elem.getBoundingClientRect();
+
+  return {
+    top: coords.top + pageYOffset,
+    left: coords.left + pageXOffset
+  };
+}
+
+var applyCurrentFilterSelector;
+
+function initializeFilters(value, sliderBlock) {
+  applyCurrentFilterSelector = value;
+  var filterSliderBlock;
+  filterSliderBlock = sliderBlock;
+  var filterPin = filterSliderBlock.querySelector('.upload-filter-level-pin');
+
+  filterPin.addEventListener('mousedown', function () {
+    var filterLevelLine = filterSliderBlock.querySelector('.upload-filter-level-line');
+    var lineCoords = getCoords(filterLevelLine);
+
+    var moveFunction = function (evt) {
+      var pinLeft = evt.pageX - lineCoords.left;
+      var rightEdge = filterLevelLine.offsetWidth - (filterPin.offsetWidth / 2);
+      var percentValue = (pinLeft / rightEdge) * 100;
+      if (percentValue < 0) {
+        percentValue = 0;
+      }
+
+      if (percentValue > 100) {
+        percentValue = 100;
+      }
+      var filterLevelValue = filterSliderBlock.querySelector('.upload-filter-level-val');
+
+      filterPin.style.left = percentValue + '%';
+      filterLevelValue.style.width = percentValue + '%';
+
+      applyCurrentFilterSelector(percentValue);
+
+    };
+
+    var upFunction = function () {
+      document.removeEventListener('mousemove', moveFunction);
+      document.removeEventListener('mouseup', upFunction);
+    };
+
+    document.addEventListener('mousemove', moveFunction);
+    document.addEventListener('mouseup', upFunction);
+
+    return false;
+  });
+}
+
+window.initializeFilters = initializeFilters;
+
